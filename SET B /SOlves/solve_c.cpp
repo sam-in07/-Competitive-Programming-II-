10a074f5b4bc9692a064daf9659fbc9184aa5a6e
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-22 17:24:37 +0600

    Create solve_c.cpp

---
+++ SET B /SOlves/solve_c.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll                 long long 
+#define maxi               1e17
+// #define MOD                1e9+7
+#define pie                3.14159265358979323846264338327
+
+#define mitsuha_wifu      ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define kyouka_chan       freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define sayonara          return 0 ;
+#define lancer_            ll isaac_foster;cin >> isaac_foster ;while(isaac_foster--) 
+
+/*
+solver : sam_in07
+date : 24/3/2024
+time : 19:28 pm
+topic : math
+
+*/
+void takumi_fujiwara() ;
+
+
+
+int main (){  
+   mitsuha_wifu ;
+ 
+    #ifndef  ONLINE_JUDGE
+       kyouka_chan ;
+    #endif 
+   
+ //lancer_   
+ {
+       takumi_fujiwara();
+}
+ return 0;
+}
+void takumi_fujiwara()   {
+  
+   ll i , n ,  mx = 0 , count = 0  ;  
+   cin >> n ; 
+//     in ;
+//     nissan {
+//        if(gcd(i,n)==1)
+//        count++;
+//     }
+     
+//       cout << max(count, mx)  <<   gtr ;
+   cout << n/2 <<  endl ;
+      for(i=1;i<n/2;i++) {
+             cout << 2 << " " ;
+              
+      }
+      if(n%2==0)
+               cout << 2 << endl ;
+                else
+                 cout << 3 << endl ;
+
+}
